1fd51275a4da8673ba0cf1afb2f82922dbfdeabd
Fenomenon199 <afeldman2025@kehillahstudent.org>
2025-01-13 08:39:08 -0800

    All caps staggering

---
+++ main.py
@@ -0,0 +1,17 @@
+# define 
+def teddy(song):
+    new_string=""
+    for character in (s):
+        if character== "a": 
+            new_string += character.upper()
+        else:
+            new_string+= character 
+        print(new_string)
+    return song 
+
+
+# call 
+print(teddy(" hold me close") )
+
+
+
